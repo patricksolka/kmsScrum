d5d8e26edd832f534e3697ffeacff53bbda98f49
Dimitrios Paliouras <dimitrios.paliouras@mni.thm.de>
2024-10-28 17:32:16 +0100

    Debug index.hmtl

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 // backend/server.js
 const express = require('express');
-const cors = require('cors'); // Ermöglicht die Kommunikation zwischen Frontend und Backend
-const Aufgabe = require('./models/aufgabe'); // Importiere die Aufgabe-Klasse
-
 const app = express();
+const cors = require('cors'); // Ermöglicht die Kommunikation zwischen Frontend und Backend
+const Aufgabe = require('./moduls/models/aufgabe'); // Importiere die Aufgabe-Klasse
+
 app.use(cors()); // CORS aktivieren, um Anfragen von anderen Hosts zu erlauben (z.B. von `localhost:3000` zum `localhost:5500`)
 app.use(express.json()); // JSON-Payloads verarbeiten
+app.use(express.static('../Frontend'));
 
 // Temporärer Speicher für Aufgaben (in-memory array)
 let aufgabenListe = [];
@@ -18,9 +19,9 @@ app.post('/api/aufgabe', (req, res) => {
     res.status(201).json(neueAufgabe); // Sende die neue Aufgabe als Antwort zurück
 });
 
-// Route, um alle Aufgaben abzurufen
-app.get('/api/aufgaben', (req, res) => {
-    res.json(aufgabenListe); // Gibt die Liste aller Aufgaben zurück
+// Startseite aufrufen
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '../Frontend/index.html');
 });
 
 // Server starten
@@ -28,3 +29,10 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Backend-Server läuft unter http://localhost:${PORT}`);
 });
+
+// Route, um alle Aufgaben abzurufen
+/*
+app.get('/api/aufgaben', (req, res) => {
+    res.json(aufgabenListe); // Gibt die Liste aller Aufgaben zurück
+});
+*/
