a56457d431dbde8bb6e4d53040692987dda548ba
Patrick <patrick.solka@mni.thm.de>
2024-11-12 19:06:39 +0100

    Added Eslint and Prettier

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,60 @@
+import globals from "globals";
+import nextPlugin from '@next/eslint-plugin-next';
+import reactPlugin from 'eslint-plugin-react';
+import hooksPlugin from 'eslint-plugin-react-hooks';
+import pluginJs from "@eslint/js";
+import tslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': hooksPlugin,
+      '@next/next': nextPlugin,
+      '@typescript-eslint': tslint,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...reactPlugin.configs['jsx-runtime'].rules,
+      ...hooksPlugin.configs.recommended.rules,
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+
+      "indent": ["error", 2],
+      "eol-last": ["error", "always"],
+      "max-len": [
+        "warn",
+        {
+          "code": 120,
+          "ignoreUrls": true,
+          "ignoreStrings": true,
+          "ignoreTemplateLiterals": true
+        }
+      ],
+      "semi": ["error", "always"],
+      "no-trailing-spaces": ["error"],
+      "arrow-body-style": ["warn", "as-needed"],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    plugins: {
+      '@typescript-eslint': tslint,
+    },
+    rules: {
+      ...tslint.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: ['.next/*'],
+  },
+];
