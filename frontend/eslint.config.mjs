9f2e0f729875d9895776041ed68da394c1540903
Patrick <patrick.solka@mni.thm.de>
2024-11-13 21:16:27 +0100

    Jest specific rules for Eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -5,13 +5,14 @@ import hooksPlugin from 'eslint-plugin-react-hooks';
 import pluginJs from "@eslint/js";
 import tslint from "@typescript-eslint/eslint-plugin"; // Ensure the plugin is properly imported
 import parser from '@typescript-eslint/parser'; // Explicitly set parser for TypeScript
+import jest from 'eslint-plugin-jest';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     languageOptions: {
-      globals: globals.browser,
+      globals: globals.browser, ...jest.environments.globals,
       parser: parser,  // Explicitly specify the parser
     },
     plugins: {
@@ -19,6 +20,7 @@ export default [
       'react-hooks': hooksPlugin,
       '@next/next': nextPlugin,
       '@typescript-eslint': tslint,
+      'jest': jest
     },
     rules: {
       ...pluginJs.configs.recommended.rules,
@@ -43,6 +45,13 @@ export default [
       "no-multiple-empty-lines": ["error", { "max": 1 }],
       "no-console": ["warn", { "allow": ["warn", "error"] }],
       "arrow-body-style": ["warn", "as-needed"],
+
+      // Jest specific rules (optional)
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
     },
     settings: {
       react: {
